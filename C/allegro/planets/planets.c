a934f00ad1d3492c750c974a9f9c968e3bfa6851
Juan Tec <32937949+koulf@users.noreply.github.com>
2019-07-24 01:50:18 -0500

    Ready to add planet menu

--- C/allegro/planets/planets.c
+++ C/allegro/planets/planets.c
@@ -5,22 +5,19 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_native_dialog.h>
  
+int menu = 0;
+
 const float FPS = 60;
 const int SCREEN_W = 640;
 const int SCREEN_H = 480;
-const int BOUNCER_SIZE = 32;
 enum MYKEYS
 {
-   KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
+   KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_ENTER
 };
 
 int main(int argc, char **argv)
-{
-   ALLEGRO_DISPLAY *display = NULL;
-   ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-   ALLEGRO_TIMER *timer = NULL;
-   ALLEGRO_BITMAP *bouncer = NULL;
-   float x = 200;
+{  
+   int x = 200;
    bool key[4] = { false, false, false, false };
    bool redraw = true;
    bool doexit = false;
@@ -30,27 +27,27 @@ int main(int argc, char **argv)
       return -1;
    }
 
+   ALLEGRO_DISPLAY* display = al_create_display(SCREEN_W, SCREEN_H);
+   if(!display) {
+      fprintf(stderr, "failed to create display!\n");
+      return -1;
+   }
+
+   ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);
+   if(!timer) {
+      fprintf(stderr, "failed to create timer!\n");
+      al_destroy_display(display);
+      return -1;
+   }
+
    if(!al_init_image_addon()) {
       al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", 
                                  NULL, ALLEGRO_MESSAGEBOX_ERROR);
-      return 0;
+      return -1;
    }
  
    if(!al_install_keyboard()) {
       fprintf(stderr, "failed to initialize the keyboard!\n");
-      return -1;
-   }
-
-   timer = al_create_timer(1.0 / FPS);
-   if(!timer) {
-      fprintf(stderr, "failed to create timer!\n");
-      return -1;
-   }
- 
-   display = al_create_display(SCREEN_W, SCREEN_H);
-   if(!display) {
-      fprintf(stderr, "failed to create display!\n");
-      al_destroy_timer(timer);
       return -1;
    }
 
@@ -60,46 +57,55 @@ int main(int argc, char **argv)
       al_show_native_message_box(display, "Error", "Error", "Failed to load image 1!", 
                                  NULL, ALLEGRO_MESSAGEBOX_ERROR);
       al_destroy_display(display);
-      return 0;
+      return -1;
    }
 
-    ALLEGRO_BITMAP* image2 = al_load_bitmap("pictures/2.png");
+   ALLEGRO_BITMAP* image2 = al_load_bitmap("pictures/2.png");
 	if(!image2)
 	{
       al_show_native_message_box(display, "Error", "Error", "Failed to load image 2!", 
                                  NULL, ALLEGRO_MESSAGEBOX_ERROR);
       al_destroy_display(display);
-      return 0;
+      return -1;
    }
 
-   bouncer = al_load_bitmap("pictures/sel.png");
-	if(!bouncer)
+   ALLEGRO_BITMAP* sel = al_load_bitmap("pictures/sel.png");
+	if(!sel)
 	{
       al_show_native_message_box(display, "Error", "Error", "Failed to load image 3!", 
                                  NULL, ALLEGRO_MESSAGEBOX_ERROR);
       al_destroy_display(display);
-      return 0;
+      return -1;
+   }
+
+   ALLEGRO_BITMAP* bg = al_load_bitmap("pictures/bg.jpg");
+	if(!bg)
+	{
+      al_show_native_message_box(display, "Error", "Error", "Failed to load background!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return -1;
    }
  
-   event_queue = al_create_event_queue();
+   ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
    if(!event_queue) {
       fprintf(stderr, "failed to create event_queue!\n");
-      al_destroy_bitmap(bouncer);
+      al_destroy_bitmap(sel);
       al_destroy_display(display);
       al_destroy_timer(timer);
       return -1;
    }
- 
+
    al_register_event_source(event_queue, al_get_display_event_source(display));
    al_register_event_source(event_queue, al_get_timer_event_source(timer));
    al_register_event_source(event_queue, al_get_keyboard_event_source());
 
    al_clear_to_color(al_map_rgb(0,0,0));
 
+   al_draw_bitmap(bg, 0, 0, 0);
    al_draw_scaled_bitmap(image, 0, 0, al_get_bitmap_width(image), al_get_bitmap_height(image), 200, 200, 100, 100, 0);
    al_draw_scaled_bitmap(image2, 0, 0, al_get_bitmap_width(image2), al_get_bitmap_height(image2), 350, 200, 100, 100, 0);
-
-   al_draw_bitmap(bouncer, x, 200, 0);
+   al_draw_bitmap(sel, x, 200, 0);
  
    al_flip_display();
  
@@ -113,11 +119,29 @@ int main(int argc, char **argv)
       if(ev.type == ALLEGRO_EVENT_TIMER)
       {
          if(key[KEY_LEFT] && x >= 350)
+         {
             x -= 150;
+            redraw = true;  
+         }
          if(key[KEY_RIGHT] && x <= 200)
+         {
             x += 150;
-         
-         redraw = true;
+            redraw = true;
+         }
+         if(key[KEY_ENTER])
+         {
+            key[KEY_ENTER] = false;
+            switch(menu)
+            {
+               case 0:
+                  menu = 1;
+                  break;
+               case 1:
+                  menu = 0;
+                  break;
+            }
+            redraw = true;
+         }
       }
       else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
          break;
@@ -132,6 +156,9 @@ int main(int argc, char **argv)
             case ALLEGRO_KEY_RIGHT:
                key[KEY_RIGHT] = true;
                break;
+            case ALLEGRO_KEY_ENTER:
+               key[KEY_ENTER] = true;
+               break;
          }
       }
       else if(ev.type == ALLEGRO_EVENT_KEY_UP)
@@ -144,8 +171,11 @@ int main(int argc, char **argv)
             case ALLEGRO_KEY_RIGHT:
                key[KEY_RIGHT] = false;
                break;
+            // case ALLEGRO_KEY_ENTER:
+            //    key[KEY_ENTER] = false;
+            //    break;
             case ALLEGRO_KEY_ESCAPE:
-               doexit = true;
+               //Pending go back action
                break;
          }
       }
@@ -153,18 +183,26 @@ int main(int argc, char **argv)
    if(redraw && al_is_event_queue_empty(event_queue))
 	{
       redraw = false;
-		al_clear_to_color(al_map_rgb(0,0,0));
+      al_clear_to_color(al_map_rgb(0,0,0));
+		al_draw_bitmap(bg, 0, 0, 0);
 		
-		al_draw_scaled_bitmap(image, 0, 0, al_get_bitmap_width(image), al_get_bitmap_height(image), 200, 200, 100, 100, 0);
-		al_draw_scaled_bitmap(image2, 0, 0, al_get_bitmap_width(image2), al_get_bitmap_height(image2), 350, 200, 100, 100, 0);
-
-      al_draw_bitmap(bouncer, x, 200, 0);
- 
+      switch(menu)
+      {
+      case 0:
+         al_draw_scaled_bitmap(image, 0, 0, al_get_bitmap_width(image), al_get_bitmap_height(image), 200, 200, 100, 100, 0);
+         al_draw_scaled_bitmap(image2, 0, 0, al_get_bitmap_width(image2), al_get_bitmap_height(image2), 350, 200, 100, 100, 0);
+         al_draw_bitmap(sel, x, 200, 0);
+         break;
+      case 1:
+         //draw planet menu
+         break;
+      }
       al_flip_display();
       }
    }
  
-   al_destroy_bitmap(bouncer);
+   al_destroy_bitmap(bg);
+   al_destroy_bitmap(sel);
    al_destroy_bitmap(image);
    al_destroy_bitmap(image2);
    al_destroy_timer(timer);
